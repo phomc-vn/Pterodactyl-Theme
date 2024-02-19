e27d680da6fca82b9dcff2591cc0dae163694401
typical <typical@phomc.vn>
2024-02-19 10:12:43 +0700

    fix: build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,12 @@
 git submodule update --init && ./scripts/applyPatches.sh || exit 1
 
 if [ "$1" == "--jar" ]; then
+    # Check if builds folder exists, if not, create it
+    if [ ! -d "builds" ]; then
+        mkdir builds
+    fi
+    
+    # Zip contents of panel-fork folder and move to builds folder
     cd panel-fork || exit 1
     zip -r ../builds/panel-fork-$(date +"%Y%m%d%H%M%S").zip *
     cd ..
